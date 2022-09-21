67d52db4b1675eb94d2494d34d14734b74718740
babsyinks <babawarunn@yahoo.com>
2022-09-21 18:59:46 +0100

    added prev and next button and their functionalities

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,21 +1,35 @@
 import React,{useState} from 'react'
 import {useLocation} from 'react-router-dom';
 import './Images.css'
+
 const Images = () => {
   const location = useLocation()
   const imagesArray = location.state
   const [imgUrl,setImgUrl] = useState('')
   const[hidden,setHidden] = useState(true)
-  const displayFullImage = (url)=>{
+  let[currentIndex, setCurrentIndex] = useState(0)
+  
+  const displayFullImage = (url,i)=>{
+    setCurrentIndex(i)
     setImgUrl(url)
     setHidden(false)
-    
   } 
+
+  const nextFullImage = ()=>{
+    setCurrentIndex(++currentIndex)
+    setImgUrl(imagesArray[currentIndex])
+  }
+
+  const prevFullImage = ()=>{
+    setCurrentIndex(--currentIndex)
+    setImgUrl(imagesArray[currentIndex])
+  }
+
   return (
     <>
       <div className='imgContainer'>
-        {imagesArray.map((url)=> (<div key={url} className='imgHolder'>
-          <img src={url} alt = 'from album' onClick={displayFullImage.bind(null,url)}/>
+        {imagesArray.map((url,i)=> (<div key={url} className='imgHolder'>
+          <img src={url} alt = 'from album' onClick={displayFullImage.bind(null,url,i)}/>
         </div>) )}
       </div>  
      
@@ -24,9 +38,15 @@ const Images = () => {
           <div className='cancelWrap'>
             <i className="fa-solid fa-circle-xmark fa-2x cancel" onClick={()=>setHidden(true)}></i>
           </div>
+          <div className='prevdiv'>
+            {currentIndex > 0 && <i className="fa-solid fa-backward fa-2x" onClick={prevFullImage}></i>}
+          </div>
           <div className='forediv'>
             <img src={imgUrl} alt='enlarged'/>
           </div> 
+          <div className='nextdiv'>
+            {currentIndex < imagesArray.length-1 && <i className="fa-solid fa-forward fa-2x" onClick={nextFullImage}></i>} 
+          </div>
         </div>  
       </div>  
     </>
